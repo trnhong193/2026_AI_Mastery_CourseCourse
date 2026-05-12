a858365ac91d2358d6c7d0325cadc889b14a87cb
Tran Hong <tranthihongleo@gmail.com>
2026-05-12 17:58:32 +0700

    D2 Conv Layer and Filters

---
+++ week10_CNNs/D2_ex1.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.ndimage import convolve # SciPy lib to perform conv on image
+
+# Load a sample grayscale image 
+image = np.random.rand(10,10)
+
+# Define convolutional kernels (filters)
+# Edge detection kernel 
+edge_kernel = np.array([
+    [-1, -1, -1],
+    [-1, -8, -1],
+    [-1, -1, -1]
+])
+
+# Blur kernel 
+blur_kernel = np.array([
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1]
+])/9
+
+# Apply convolution
+egde_image = convolve(image, edge_kernel)
+blur_image = convolve(image, blur_kernel)
+
+# Visualize original and filterd images 
+fig, axes = plt.subplots(1, 3, figsize = (12,4))
+axes[0].imshow(image, cmap="gray")
+axes[0].set_title("Original Image")
+axes[1].imshow(egde_image, cmap = "gray")
+axes[1].set_title("Edged Detection Image")
+axes[2].imshow(blur_image, cmap = "gray")
+axes[2].set_title("Blurred Image")
